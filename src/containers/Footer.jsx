79c7ebfa4a8b231e10d9241cfcf6ee2ed80e8697
anugrahprathap <anugrahprathap@gmial.com>
2023-12-11 00:45:17 +0530

    v2:latest

--- src/containers/Footer.jsx
+++ src/containers/Footer.jsx
@@ -8,17 +8,17 @@ function Footer() {
   return (
     <Row className="bg-dark text-white p-4">
       <Col xs={4} className="text-center">
-        <a href="https://twitter.com/your_twitter_handle" target="_blank" className="text-white">
+        <a href="https://twitter.com/anugrahprathap" target="_blank" className="text-white">
           <FontAwesomeIcon icon={faTwitter} size="2x" />
         </a>
       </Col>
       <Col xs={4} className="text-center">
-        <a href="https://linkedin.com/in/your_linkedin_profile" target="_blank" className="text-white">
+        <a href="https://linkedin.com/in/anugrahprathap" target="_blank" className="text-white">
           <FontAwesomeIcon icon={faLinkedin} size="2x" />
         </a>
       </Col>
       <Col xs={4} className="text-center">
-        <a href="https://github.com/your_github_username" target="_blank" className="text-white">
+        <a href="https://github.com/anugrahprathap" target="_blank" className="text-white">
           <FontAwesomeIcon icon={faGithub} size="2x" />
         </a>
       </Col>
